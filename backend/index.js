a155519ad6d8567c488c17db510400c492981775
@ttractivenes$ <moriahbuckridge96115@gmail.com>
2023-12-25 16:32:05 +0800

    changed server url with netlify app url

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
-
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server({
-    cors: "http://localhost:5173/"
-})
-io.on('connection', function (socket) {
-
-    socket.on('canvasImage', (data) => {
-        socket.broadcast.emit('canvasImage', data);
-    });
+  cors: "https://collaborative.react.netlify.app/",
+});
+io.on("connection", function (socket) {
+  socket.on("canvasImage", (data) => {
+    socket.broadcast.emit("canvasImage", data);
+  });
 });
 
 io.listen(5000);
